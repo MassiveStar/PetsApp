362cf06447bc0b156ee7b0cfc0f23effaaae55ba
Elvis Franks Condori Arias <massivestar7@gmail.com>
2021-05-02 22:46:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/petsapp25042021/ui/PetsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petsapp25042021.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petsapp25042021.R
+
+class PetsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pets)
+    }
+}
