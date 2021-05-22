d9decbd8f1ea9cf1864007a22f7f9848c6a5959e
Elvis Franks Condori Arias <massivestar7@gmail.com>
2021-05-22 07:22:29 -0500

    Conf AppBarLayout

--- app/src/main/java/com/example/petsapp25042021/ui/PetsActivity.kt
+++ app/src/main/java/com/example/petsapp25042021/ui/PetsActivity.kt
@@ -1,12 +1,35 @@
 package com.example.petsapp25042021.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.petsapp25042021.R
+import com.example.petsapp25042021.databinding.ActivityPetsBinding
 
 class PetsActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityPetsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pets)
+
+        binding = ActivityPetsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
